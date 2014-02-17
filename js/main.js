ad6bedb0132406da7396abed6c3a9346f3381fbf
Brian Duimstra <brianduimstra@brians-mbp.home>
2014-02-16 23:13:19 -0800

    Mobile layout complete

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function() {
+	$('.mobile-nav').on('click', function() {
+		if ($('nav ul').is(':visible')) {
+			$('nav ul').slideUp(200);
+		} else {
+			$('nav ul').slideDown(200);
+		}
+	});
+});
